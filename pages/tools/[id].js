42beadc3205f20ff7f6c1f7e7045c0ab042df8e6
Carsten Pleiser <carstenpleiser@MacBook-Pro-14.local>
2022-05-26 13:42:33 +0100

    Started working on dynamic routes

--- pages/tools/[id].js
+++ pages/tools/[id].js
@@ -5,20 +5,6 @@ export default function Tool() {
   return <h1>Tool</h1>;
 }
 
-export async function getStaticPaths() {
-  const paths = await table.select(params);
-  console.log(paths);
-  return {
-    paths,
-    fallback: false,
-  };
-}
+export async function getStaticPaths() {}
 
-export async function getStaticProps({ params }) {
-  const toolData = getToolData(params.id);
-  return {
-    props: {
-      toolData,
-    },
-  };
-}
+export async function getStaticProps({ params }) {}
