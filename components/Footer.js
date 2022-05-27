9ce59dfa2de866b94fd2b6a2268f47b48e4e7d72
Carsten Pleiser <carstenpleiser@MacBook-Pro-14.local>
2022-05-27 14:50:25 +0100

    Added Twitter badge

--- components/Footer.js
+++ components/Footer.js
@@ -8,26 +8,16 @@ export default function Footer() {
         <div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
           <ul className="flex flex-wrap items-center space-x-4 sm:space-x-8">
             <li>
-              <a rel="noopener noreferrer" href="#">
-                Terms of Use
-              </a>
-            </li>
-            <li>
-              <a rel="noopener noreferrer" href="#">
-                Privacy
+              <a
+                rel="noopener noreferrer"
+                href="https://airtable.com/shrkB7Vo1eklM96Jq"
+              >
+                Suggest a tool
               </a>
             </li>
           </ul>
         </div>
         <ul className="flex flex-wrap pl-3 space-x-4 justify-right sm:space-x-8">
-          <li>
-            <a
-              rel="noopener noreferrer"
-              href="https://airtable.com/shrkB7Vo1eklM96Jq"
-            >
-              Submit
-            </a>
-          </li>
           <li>
             <a
               href="https://twitter.com/ckpleiser?ref_src=twsrc%5Etfw"
