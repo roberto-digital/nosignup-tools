26b0b3b9bd99bc5f030adadf18cbd06111c34177
Carsten Pleiser <carstenpleiser@MacBook-Pro-14.local>
2022-05-30 11:49:03 +0100

    Added Mailchimp Subscription

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -17,13 +17,14 @@ const NewsletterForm = () => {
     } catch (e) {
       setErrorMessage(e.response.data.error);
       setState("ERROR");
+      setEmail("");
     }
   };
 
   return (
-    <div className="p-6 md:w-full xl:w-full mx-auto flex flex-col xl:items-stretch justify-center xl:flex-row">
-      <div className="w-full xl:w-7/12 xl:pl-40 xl:py-28">
-        <h1 className="text-2xl md:text-4xl xl:text-5xl  font-bold leading-tight text-gray-700 mb-4 text-center md:mt-0 mt-4">
+    <div className="p-6 mx-auto flex flex-col xl:items-stretch xl:flex-row  justify-center">
+      <div className="w-full xl:w-7/12 xl:px-20 xl:py-28">
+        <h1 className="text-2xl md:text-4xl xl:text-5xl font-bold leading-tight text-gray-700 mb-4 text-center md:mt-0 mt-4">
           Fresh No-Signup Tools Delivered To Your Inbox
         </h1>
         <p className="text-lg leading-normal text-gray-700  text-center">
@@ -35,6 +36,7 @@ const NewsletterForm = () => {
             onChange={(event) => setEmail(event?.target?.value ?? "")}
             className="bg-gray-100 rounded-lg rounded-r-none text-base leading-none text-gray-800 focus:text-gray-900 dark:text-white p-5 w-4/5 border border-transparent focus:outline-none focus:border-gray-500"
             type="email"
+            value={email}
             placeholder="john.doe@gmail.com"
           />
 
