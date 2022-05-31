4b21f99c414bb1d2c1e01c0d7c6993bd28b36d47
Carsten Pleiser <carstenpleiser@MacBook-Pro-14.local>
2022-05-31 09:08:53 +0100

    Added OG metadata to detail page for SEO

--- components/Layout.js
+++ components/Layout.js
@@ -6,38 +6,6 @@ import Head from "next/head";
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        <title>No Signup Tools</title>
-        <meta property="og:title" content="No-Signup Tools" key="title" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
-
-        <meta
-          name="description"
-          content="Free web apps that don't require registration or login"
-        />
-
-        <meta property="og:url" content="https://www.nosignup.tools/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="No-Signup Tools" />
-        <meta
-          property="og:description"
-          content="Free web apps that don't require registration or login"
-        />
-        <meta property="og:image" content="/images/nosignup_tools.jpeg" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="nosignup.tools" />
-        <meta property="twitter:url" content="https://www.nosignup.tools/" />
-        <meta name="twitter:title" content="No-Signup Tools" />
-        <meta
-          name="twitter:description"
-          content="Free web apps that don't require registration or login"
-        />
-        <meta name="twitter:image" content="/images/nosignup_tools.jpeg" />
-      </Head>
       <NavSearchMenu />
       <main className="flex-1 lg:pl-12 py-6 px-6 lg:px-0 justify-center align-center">
         {children}
--- pages/tools/[id].js
+++ pages/tools/[id].js
@@ -6,6 +6,7 @@ import SimpleCard from "../../components/SimpleCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Tool({ toolData }) {
   const { tools } = useContext(ToolsContext);
@@ -45,81 +46,128 @@ export default function Tool({ toolData }) {
   });
 
   return (
-    <section className="text-gray-700 body-font overflow-hidden  bg-white h-full">
-      <Link href="/">
-        <a className="underline text-blue-700 hover:text-blue-900">
-          Back to results page
-        </a>
-      </Link>
-      <div className="container px-5 pt-24 mx-auto">
-        <div className="lg:w-4/5 mx-auto flex flex-wrap">
-          <img
-            alt={`${toolData.fields.name} Screenshot`}
-            className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
-            src={toolData.fields.screenshot}
-          />
+    <>
+      <Head>
+        <title>{toolData.fields.name} - No-Signup Tools</title>
+        <meta
+          property="og:title"
+          content={`${toolData.fields.name} - No-Signup Tools`}
+          key="title"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
 
-          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              {toolData.fields.category}
-            </h2>
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-              {toolData.fields.name}
-            </h1>
-            <div className="flex mb-4">
-              <span className="flex items-center ">
-                <div className="flex items-center text-blue-900">
-                  {[...Array(toolData.fields.rating)].map((e, i) => (
-                    <FontAwesomeIcon key={i} icon={faStar} className="mr-2" />
-                  ))}
-                </div>
-              </span>
-              <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
-                <a
-                  className="ml-2 text-gray-500"
-                  href={`https://twitter.com/intent/tweet?text=I%20just%20found%20${toolData.fields.url}%20on%20nosignup.tools`}
-                >
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
+        <meta name="description" content={`${toolData.fields.description}`} />
+
+        <meta
+          property="og:url"
+          content={`https://nosignup.tools/tools/${toolData.fields.name
+            .toLowerCase()
+            .replace(/ /g, "-")}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content={`${toolData.fields.description}`}
+        />
+        <meta property="og:image" content="/images/nosignup_tools.jpeg" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="nosignup.tools" />
+        <meta
+          property="twitter:url"
+          content={`https://nosignup.tools/tools/${toolData.fields.name
+            .toLowerCase()
+            .replace(/ /g, "-")}`}
+        />
+        <meta
+          name="twitter:title"
+          content={`${toolData.fields.name} - No-Signup Tools`}
+        />
+        <meta
+          name="twitter:description"
+          content={`${toolData.fields.description}`}
+        />
+        <meta name="twitter:image" content="/images/nosignup_tools.jpeg" />
+      </Head>
+      <section className="text-gray-700 body-font overflow-hidden  bg-white h-full">
+        <Link href="/">
+          <a className="underline text-blue-700 hover:text-blue-900">
+            Back to results page
+          </a>
+        </Link>
+        <div className="container px-5 pt-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <img
+              alt={`${toolData.fields.name} Screenshot`}
+              className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
+              src={toolData.fields.screenshot}
+            />
+
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                {toolData.fields.category}
+              </h2>
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                {toolData.fields.name}
+              </h1>
+              <div className="flex mb-4">
+                <span className="flex items-center ">
+                  <div className="flex items-center text-blue-900">
+                    {[...Array(toolData.fields.rating)].map((e, i) => (
+                      <FontAwesomeIcon key={i} icon={faStar} className="mr-2" />
+                    ))}
+                  </div>
+                </span>
+                <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+                  <a
+                    className="ml-2 text-gray-500"
+                    href={`https://twitter.com/intent/tweet?text=I%20just%20found%20${toolData.fields.url}%20on%20nosignup.tools`}
                   >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-              </span>
-            </div>
-            <p className="leading-relaxed">{toolData.fields.description}</p>
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
+              <p className="leading-relaxed">{toolData.fields.description}</p>
 
-            <div className="flex">
-              <Button
-                className="mt-3"
-                size="base"
-                text={`Try ${toolData.fields.name}`}
-                link={toolData.fields.url}
-              />
+              <div className="flex">
+                <Button
+                  className="mt-3"
+                  size="base"
+                  text={`Try ${toolData.fields.name}`}
+                  link={toolData.fields.url}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="container px-5 pt-24 mx-auto">
-        <div className="lg:w-4/5 mx-auto flex flex-wrap">
-          {RecommendedTools.length ? (
-            <h2 className="text-gray-900 text-2xl title-font font-bold">
-              Other Recommended {`${toolData.fields.category}`} Tools
-            </h2>
-          ) : (
-            ""
-          )}
-          <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-6 mt-10">
-            {RecommendedTools}
+        <div className="container px-5 pt-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            {RecommendedTools.length ? (
+              <h2 className="text-gray-900 text-2xl title-font font-bold">
+                Other Recommended {`${toolData.fields.category}`} Tools
+              </h2>
+            ) : (
+              ""
+            )}
+            <div className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-6 mt-10">
+              {RecommendedTools}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
