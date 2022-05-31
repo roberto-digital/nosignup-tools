5065391257d1d1d544999b656166a6b78e11714a
Carsten Pleiser <carstenpleiser@MacBook-Pro-14.local>
2022-05-31 13:00:06 +0100

    Added Plausible Analytics

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const { withPlausibleProxy } = require("next-plausible");
+
+const nextConfig = withPlausibleProxy()({
   reactStrictMode: true,
   images: {
     domains: ["dl.airtable.com"],
   },
-};
+});
 
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Script from "next/script";
 
 import "../styles/globals.css";
+import PlausibleProvider from "next-plausible";
 import { ToolsProvider } from "../contexts/ToolsContext";
 import Layout from "../components/Layout";
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -9,15 +10,17 @@ config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ToolsProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <Script
-        src="https://zink.tips/zwc.js?rid=6295f658be9816218b32a427&c=purple"
-        strategy="lazyOnload"
-      />
-    </ToolsProvider>
+    <PlausibleProvider domain="nosignup.tools">
+      <ToolsProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <Script
+          src="https://zink.tips/zwc.js?rid=6295f658be9816218b32a427&c=purple"
+          strategy="lazyOnload"
+        />
+      </ToolsProvider>
+    </PlausibleProvider>
   );
 }
 
