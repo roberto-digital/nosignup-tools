9eb47444b9ffb0ac6e2233a3951f4398a25d225b
Carsten Pleiser <carstenpleiser@MacBook-Pro-14.local>
2022-06-14 21:26:14 +0100

    Added Buy me a Coffee in footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,11 +5,11 @@ export default function Footer() {
   return (
     <footer className="text-gray-900 body-font w-full">
       <div className="bg-purple">
-        <div className="py-6 px-6 flex items-center justify-left sm:justify-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center justify-center text-gray-100">
-            <span className="ml-3 text-xl font-bold">nosignup.tools</span>
+        <div className="py-6 px-6 flex items-center justify-center sm:justify-center sm:flex-row flex-col">
+          <a className="flex title-font font-medium text-gray-100">
+            <span className="mx-3 text-xl font-bold">nosignup.tools</span>
           </a>
-          <p className="text-sm text-gray-500 sm:mt-0 ml-3 mt-4">
+          <p className="text-sm text-gray-500 sm:mt-0 mx-3 mt-4">
             <a
               href="https://twitter.com/ckpleiser?ref_src=twsrc%5Etfw"
               className="twitter-follow-button"
@@ -22,6 +22,15 @@ export default function Footer() {
               strategy="lazyOnload"
             />
           </p>
+
+          <a
+            className="mx-3 text-white bg-yellow sm:mt-0 mt-4 hover:underline"
+            href="https://www.buymeacoffee.com/cpleiser"
+            target="_blank"
+          >
+            <span>☕ </span>
+            Buy me a coffee
+          </a>
         </div>
       </div>
     </footer>
--- pages/_app.js
+++ pages/_app.js
@@ -15,19 +15,6 @@ function MyApp({ Component, pageProps }) {
         <Layout>
           <Component {...pageProps} />
         </Layout>
-        <Script
-          src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
-          data-name="bmc-button"
-          data-slug="cpleiser"
-          data-color="#FFDD00"
-          data-emoji=""
-          data-font="Cookie"
-          data-text="Buy me a coffee"
-          data-outline-color="#000000"
-          data-font-color="#000000"
-          data-coffee-color="#ffffff"
-          strategy="beforeInteractive"
-        />
         <Script src="https://tally.so/widgets/embed.js" />
       </ToolsProvider>
     </PlausibleProvider>
